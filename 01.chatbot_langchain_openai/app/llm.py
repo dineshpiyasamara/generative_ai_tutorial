3ea21b4749f1bda3db6294454e6e248dbf071352
Dinesh <dineshpiyasamara@gmail.com>
2024-06-23 13:03:37 +0530

    update llm

--- 01.chatbot_langchain_openai/app/llm.py
+++ 01.chatbot_langchain_openai/app/llm.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from langchain_openai import AzureChatOpenAI
+from langchain_openai import AzureChatOpenAI, ChatOpenAI
 
 
 def get_llm():
@@ -14,4 +14,14 @@ def get_llm():
         openai_api_version=os.getenv("OPENAI_API_VERSION"),
         azure_deployment=os.getenv("AZURE_DEPLOYMENT")
     )
+
+    # # Set OpenAI API key
+    # os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
+
+    # # Initialize the ChatOpenAI model
+    # llm = ChatOpenAI(
+    #     model="gpt-3.5-turbo",
+    #     temperature=0
+    # )
+
     return llm
